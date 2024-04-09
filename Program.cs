fb25f6ddc6c79a401dd7eab493bffc57bac15187
BaoHG <baohuynh291@gmail.com>
2024-04-09 21:18:46 +0700

    add
    add

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@
         }
         int maxValue = 0;
         int sum = 0;
+        int sum_diagonal = 0;
         for (int row = 0; row < myArray.GetLength(0); row++)
         {
             for (int col = 0; col < myArray.GetLength(1); col++)
@@ -30,9 +31,14 @@
                 {
                     maxValue = myArray[row, col];
                 }
+                if (row == col)
+                {
+                    sum_diagonal += myArray[row, col];
+                }
             }
         }
         Console.WriteLine($"Gia tri lon nhat cua mang: {maxValue}");
         Console.WriteLine($"Gia tri tong la: {sum}");
+        Console.WriteLine($"Tong duong cheo chinh: {sum_diagonal}");
     }
 }
